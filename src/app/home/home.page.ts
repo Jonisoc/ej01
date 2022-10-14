9fa6ba0244dd07e651370cefaaf3a40eeb23a9d7
AntAlvMtz <andealvaradoma@ittepic.edu.mx>
2022-10-13 23:38:27 -0600

    Primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public photos = [];
+
+  constructor() {
+    this.photos = [
+      {
+        name: "Foto 1",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati commodi quisquam tempora! Enim mollitia qui quibusdam voluptatibus, atque ullam! Deleniti laudantium libero quod deserunt temporibus adipisci quos illum nisi aperiam!",
+        url: "https://picsum.photos/id/237/200/300"
+      },
+      {
+        name: "Foto 2",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati commodi quisquam tempora! Enim mollitia qui quibusdam voluptatibus, atque ullam! Deleniti laudantium libero quod deserunt temporibus adipisci quos illum nisi aperiam!",
+        url: "https://picsum.photos/id/238/200/300"
+      },
+      {
+        name: "Foto 3",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati commodi quisquam tempora! Enim mollitia qui quibusdam voluptatibus, atque ullam! Deleniti laudantium libero quod deserunt temporibus adipisci quos illum nisi aperiam!",
+        url: "https://picsum.photos/id/239/200/300"
+      }
+    ]
+  }
 
 }
